bf3f72230cbb3238c614edc058d90db6498a4fef
KV Mahesh <kvmahesh40@gmail.com>
2021-08-08 22:39:43 +0530

    to check an array is sorted or not?

---
+++ Arrays/array_sorted.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool arr_sorted(int arr[], int n){
+
+for(int i=1;i<n;i++){
+    if(arr[i]<arr[i-1])
+        return false;
+}
+return true;
+
+}
+int main(){
+
+int arr[] = {5,8,0,100};
+int n = sizeof(arr)/sizeof(arr[0]);
+printf("%s", arr_sorted(arr,n)? "true":"false");
+}
