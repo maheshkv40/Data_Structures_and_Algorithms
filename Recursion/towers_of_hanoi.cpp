e3a2c0137c6228717e20cd53c7cfc12aebb19aac
KV Mahesh <kvmahesh40@gmail.com>
2021-08-10 22:16:43 +0530

    towes of Hanoi

---
+++ Recursion/towers_of_hanoi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+void Tower_of_hanoi(int n, char A, char B, char C)
+{
+    if (n == 1)
+    {
+        cout<<"Move 1 from " <<  A << " to " << C << endl;
+        return;
+    }
+    Tower_of_hanoi(n-1, A, C, B);
+    cout<<"Move " << n << " from " <<  A << " to " << C << endl;
+    Tower_of_hanoi(n-1, B, A, C);
+}
+
+int main() {
+
+	int n = 3;
+    Tower_of_hanoi(n, 'A', 'B', 'C');
+	return 0;
+}
