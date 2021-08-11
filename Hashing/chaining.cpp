c96a43148977518c93fc3513d4c5fdc29fc6203c
KV Mahesh <kvmahesh40@gmail.com>
2021-08-11 12:25:18 +0530

    added chaing.cpp

---
+++ Hashing/chaining.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct chain{
+    int bucket;
+    list <int>* table;
+    chain(int b){
+        bucket =b;
+        table = new list<int>[bucket];
+    }
+    void insert(int key){
+        int i = key %bucket;
+        table[i].push_back(key);
+    }
+    void del(int key){
+        int i = key % bucket;
+        table[i].remove(key);
+    }
+    bool search(int key){
+        int i = key %bucket;
+        for(auto x: table[i])
+            if(x== key)
+                return true;
+        return false;
+    }
+
+};
+
+int main(){
+    chain ch(7);
+    ch.insert(10);
+    ch.insert(20);
+    ch.insert(15);
+    ch.insert(7);
+    cout << ch.search(10) << endl;
+    ch.del(15);
+    cout << ch.search(15);
+    return 0;
+}
