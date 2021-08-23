6ddd353c1c05c14288b0f7d7b644eb3d13848646
KV Mahesh <kvmahesh40@gmail.com>
2021-08-23 11:10:53 +0530

    Circular linked list insert at begin

---
+++ Linked_List/circular_ll_ins_beg.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    if(head == NULL)
+        return;
+    Node *p =head;
+    do{
+        cout<<p->data<<"->";
+        p= p->next;
+    }while(p!=head);
+
+}
+
+Node *cll_insertBegin(Node * head,int x){
+    Node *temp = new Node(x);
+    if(head == NULL){
+        temp->next =temp;
+        return temp;
+    }
+    else{
+        Node *curr = head;
+        while(curr->next!=head){
+            curr =curr->next;
+        }
+        curr->next = temp;
+        temp->next = head;
+    }
+    return temp;
+
+}
+
+int main()
+{
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	head->next->next->next=head;
+	printlist(head);
+	cout<<endl;
+	head=cll_insertBegin(head,5);
+	printlist(head);
+	return 0;
+}
