ba1a167458f28c40c894c74a6d9e72c94185fe75
KV Mahesh <kvmahesh40@gmail.com>
2021-08-23 11:25:45 +0530

    Circular linked list optimal insert at begin

---
+++ Linked_List/circular_ll_ins_beg_optimal.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    if(head == NULL)
+        return;
+    Node *p =head;
+    do{
+        cout<<p->data<<"->";
+        p= p->next;
+    }while(p!=head);
+
+}
+
+Node *cll_insertBegin_optimal(Node * head,int x){
+    Node *temp = new Node(x);
+    if(head == NULL){
+        temp->next =temp;
+        return temp;
+    }
+    else{
+        temp->next = head->next;
+        head->next = temp;
+        int t = head->data;
+        head->data = temp->data;
+        temp->data = t;
+        return head;
+
+    }
+
+}
+
+int main()
+{
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	head->next->next->next=head;
+	printlist(head);
+	cout<<endl;
+	head=cll_insertBegin_optimal(head,5);
+	printlist(head);
+	return 0;
+}
