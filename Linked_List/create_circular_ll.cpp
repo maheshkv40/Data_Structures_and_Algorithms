05a7fc0924970f3e2c58600730809fca5cef8987
KV Mahesh <kvmahesh40@gmail.com>
2021-08-23 10:43:57 +0530

    Create a circular linked list

---
+++ Linked_List/create_circular_ll.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int d){
+        d = data;
+        next = NULL;
+    }
+
+};
+
+
+int main(){
+
+ Node *head = new Node(10);
+ head->next = new Node(20);
+ head->next->next = new Node(30);
+ head->next->next->next = new Node(40);
+ head->next->next->next->next =head;
+ return 0;
+
+
+
+}
