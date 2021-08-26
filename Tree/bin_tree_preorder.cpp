b40acefc2ead1c25cf912f5fa68194061a3222c7
KV Mahesh <kvmahesh40@gmail.com>
2021-08-26 22:35:22 +0530

    preorder traversal of binary tree

---
+++ Tree/bin_tree_preorder.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int key;
+    Node *left;
+    Node*right;
+    Node(int k){
+        key =k;
+        left=NULL;
+        right=NULL;
+    }
+
+};
+
+void preorder_traversal(Node *root){
+    if(root == NULL){
+        return;
+    }
+    cout<<root->key<<" ";
+    preorder_traversal(root->left);
+    preorder_traversal(root->right);
+
+}
+
+
+int main(){
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+    root->right->left = new Node(60);
+    root->right->right = new Node(70);
+    preorder_traversal(root);
+    return 0;
+}
