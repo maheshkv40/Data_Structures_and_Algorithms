c6d35efae40f84a4e144fab681a80c6d7a7a50f6
KV Mahesh <kvmahesh40@gmail.com>
2021-08-27 21:04:53 +0530

    Size of a binary tree

---
+++ Tree/binary_tree_size.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int key;
+    Node *left;
+    Node*right;
+    Node(int k){
+        key =k;
+        left=NULL;
+        right=NULL;
+    }
+
+};
+
+int getsize(Node *root){
+    if(root == NULL){
+        return 0;
+    }
+    else{
+        return 1+getsize(root->left)+getsize(root->right);
+    }
+}
+
+
+int main(){
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+    root->right->left = new Node(60);
+    root->right->right = new Node(70);
+    cout<<getsize(root);
+    return 0;
+}
