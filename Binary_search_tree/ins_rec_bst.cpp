66696bfb216d2991c683be2fffa55a95706fb2f1
KV Mahesh <kvmahesh40@gmail.com>
2021-08-29 12:35:29 +0530

    BST insertion recursive

---
+++ Binary_search_tree/ins_rec_bst.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node * left;
+    Node *right;
+    Node(int d){
+        data =d;
+        left =NULL;
+        right =NULL;
+    }
+
+};
+
+Node *rec_ins(Node *root, int x){
+    Node *temp =new Node(x);
+    if(root == NULL){
+        return temp;
+    }
+    if(root->data < x){
+        root->right = rec_ins(root->right,x);
+    }
+    else if(root->data > x){
+        root->left = rec_ins(root->left,x);
+    }
+    return root;
+}
+
+void inorder(Node *root){
+    if(root !=NULL){
+        inorder(root->left);
+        cout<<root->data<< " ";
+        inorder(root->right);
+    }
+}
+
+int main(){
+
+    Node *root = new Node(30);
+    root->left = new Node(20);
+    root->right = new Node(40);
+    root->left->left = new Node(10);
+    root->left->right = new Node(25);
+    root->right->left = new Node(35);
+    root->right->right = new Node(45);
+    int x =37;
+    root = rec_ins(root,x);
+    inorder(root);
+    return 0;
+}
